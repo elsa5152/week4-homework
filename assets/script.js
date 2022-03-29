e928b8cea2987ec4057e5063594845389908544e
Elsa <ljq5152@gmail.com>
2022-03-29 14:48:56 +1030

    js 1st update

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,18 @@
+ const startButton =document.getElementById("start-btn")
+ const questionContainerElemengt = document.getElementById("question-container")
 
+ const shuffledQuestions, currentQuestionIndex
+
+ startButton. addEventListener("click", startQuiz)
+
+ //we need 3 function to execute the app.//
 function startQuiz() {
+    console.log("Started")
+    startButton.classList.add("hide")
+    shuffledQuestions = question.sort(()=> Math.random() - .5)
+    currentQuestionIndex = 0
+    questionContainerElemengt.classList.remove("hide")
+    setNextQuestion()
 
 }
 
@@ -10,3 +23,13 @@ function setNextQuestion() {
 function selectAnswer() {
 
 }
+
+const question = [
+    {
+        questiong:"what is 2+2",
+        answer: [
+            {text: "4", correct: ture },
+            {text: "22", correct: false }
+        ]
+    }
+]
